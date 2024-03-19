3f9ea61bddb72a2f06afc834bc4f7ed982cf9dc9
nosikmy <91544344+nosikmy@users.noreply.github.com>
2024-03-20 00:28:37 +0700

    work worked

--- src/main/java/ru/nsu/inheritance/MultipleInheritancer.java
+++ src/main/java/ru/nsu/inheritance/MultipleInheritancer.java
@@ -4,6 +4,7 @@ import net.sf.cglib.proxy.Enhancer;
 import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
 import ru.nsu.inheritance.annotations.Extends;
+import ru.nsu.inheritance.annotations.RootInterface;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
@@ -30,9 +31,9 @@ import java.util.List;
  * </p>
  */
 public class MultipleInheritancer implements MethodInterceptor {
-    private static final Deque<Class<?>> queue = new ArrayDeque<>();
     private static Deque<Class<?>> resultingQueue = new ArrayDeque<>();
     private static boolean abstractFlag = false;
+    private static Class<?> rootInterface = null; // Возникает проблема, если у пользователя два дерева с разными RootInterface
 
     /**
      * Method to generate class bases on provided "clazz" with multiple inheritance possibility.
@@ -46,6 +47,20 @@ public class MultipleInheritancer implements MethodInterceptor {
         Enhancer enhancer = new Enhancer();
         enhancer.setSuperclass(clazz);
         enhancer.setCallback(new MultipleInheritancer());
+        if (!abstractFlag) {
+            for (Class<?> i : clazz.getInterfaces()) {
+                if (i.getAnnotation(RootInterface.class) != null) {
+                    if (rootInterface != null) {
+                        throw new RuntimeException("The class has several root interfaces");
+                    } else {
+                        rootInterface = i;
+                    }
+                }
+            }
+            if (rootInterface == null) {
+                throw new RuntimeException("The class doesn't have a root interface");
+            }
+        }
         return (T) enhancer.create();
     }
 
@@ -64,13 +79,14 @@ public class MultipleInheritancer implements MethodInterceptor {
      */
     @Override
     public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+        // тут нужна обработка ошибок
         if (abstractFlag) {
             abstractFlag = false;
             return proxy.invokeSuper(obj, args);
         }
         callNextMethod(obj, false);
 
-        if (queue.isEmpty() && !resultingQueue.isEmpty()) {
+        if (!resultingQueue.isEmpty()) {
             if (method.getReturnType().equals(void.class)) {
                 for (Class<?> c : resultingQueue) {
                     Method m = c.getMethod(method.getName(), method.getParameterTypes());
@@ -118,7 +134,7 @@ public class MultipleInheritancer implements MethodInterceptor {
         List<Class<?>> extendsClasses =
                 Arrays.stream(obj.getClass().getSuperclass().getAnnotation(Extends.class).value()).toList();
 
-        addToQueue(extendsClasses);
+        Deque<Class<?>> queue = addToQueue(extendsClasses);
 
         while (!queue.isEmpty()) {
             Class<?> aClass = queue.pollFirst();
@@ -139,11 +155,16 @@ public class MultipleInheritancer implements MethodInterceptor {
      *
      * @param classes classes to add
      */
-    private static void addToQueue(List<Class<?>> classes) {
+    private static Deque<Class<?>> addToQueue(List<Class<?>> classes) {
+        Deque<Class<?>> queue = new ArrayDeque<>();
         classes.forEach(cl -> {
+            if (Arrays.stream(cl.getInterfaces()).noneMatch(i -> i != rootInterface)) {
+                throw new RuntimeException("Parent class doesn't have a root interface");
+            }
             if (!queue.contains(cl)) {
                 queue.addLast(cl);
             }
         });
+        return queue;
     }
 }
